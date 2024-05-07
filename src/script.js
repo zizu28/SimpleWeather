c31bb3aaebb45559377dd5e7901290d37ef6ac9b
zizu28 <ziblimzulka.zz@gmail.com>
2024-05-08 05:58:55 +0700

    Logic manipulation done

--- src/script.js
+++ src/script.js
@@ -12,13 +12,36 @@ const hourlyDate = document.querySelector('.date');
 const header = document.createElement('thead');
 const headerRow = document.createElement('tr');
 const currentDataContainer = document.querySelector('#currentData');
-const currentDataHeader = document.querySelector('#currentDataHeader');
-const currentDataDateLocationCountry = document.querySelector('#currentDataDateLocationCountry');
-const currentDataTempCondition = document.querySelector('#currentDataTempCondition');
-const currentDataMetrics = document.querySelector('#currentDataMetrics');
+const currentDataDate = document.querySelector('.currentDataDate');
+const currentDataLocation = document.querySelector('.currentDataLocation');
+const currentDataCountry = document.querySelector('.currentDataCountry');
+const currentTemp = document.querySelector('.currentTemp');
+const currentFeeling = document.querySelector('.currentFeeling');
+const currentDataIcon = document.querySelector('#currentDataIcon');
+const currentCondition = document.querySelector('.currentCondition');
+const speed = document.querySelector('.speed');
+const speedText = document.querySelector('.speedText');
+const percent = document.querySelector('.percent');
+const percentText = document.querySelector('.percentText');
+const uv = document.querySelector('.uv');
+const uvText = document.querySelector('.uvText');
+const visibility = document.querySelector('.visibility');
+const visibilityText = document.querySelector('.visibilityText');
+const currentDataHeaderTitle = document.querySelector('.currentDataHeaderTitle');
+const currentDataHeaderTime = document.querySelector('.currentDataHeaderTime');
+const currentDataHeaderUpdate = document.querySelector('.currentDataHeaderUpdate');
+const dailyForecast = document.querySelector('.dailyForecast');
+const forecastDayAndDate = document.querySelector('.forecastDayAndDate');
+const dailyIcon = document.querySelector('.dailyIcon');
+const high = document.querySelector('.high');
+const low = document.querySelector('.low');
+
+
 
 let city;
 hourlyForecast.style.display = "none";
+currentDataContainer.style.display = 'none';
+dailyForecast.style.display = 'none';
 
 search.addEventListener('change', () => {
     city = search.value;
@@ -35,24 +58,84 @@ celcius.addEventListener('click', () => {
         hourlyDate.textContent = [dateArray[1], dateArray[2], dateArray[3]].join('-')
 
         // CURRENT DATA LOGIC
-        const currentDataHeaderTitle = document.querySelector('.currentDataHeaderTitle');
-        const currentDataHeaderTimeUpdate = document.querySelector('.currentDataHeaderTimeUpdate');
-        const currentDataHeaderTime = document.querySelector('.currentDataHeaderTime');
-        const currentDataHeaderUpdate = document.querySelector('.currentDataHeaderUpdate');
+        currentDataContainer.style = 'display: grid';
+        dailyForecast.style.display = 'grid';
+        
         
         const locationData = resolvedData.location.localtime.toString().split(' ');
         currentDataHeaderTime.textContent = locationData[1]
+        currentDataHeaderTime.style = 'text-align: center;';
+        currentDataHeaderUpdate.textContent = 'Last Updated';
+        currentDataHeaderUpdate.style = 'opacity: 0.5; font-weight: bold;';
         currentDataHeaderTitle.textContent = 'Current Weather';
 
+        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        currentDataDate.textContent = days[date.getDay() - 1] + ', '+ [dateArray[1], dateArray[2]].join(' ');
+        currentDataLocation.textContent = resolvedData.location.name + ', ' + resolvedData.location.region;
+        currentDataCountry.textContent = resolvedData.location.country;
+        currentDataCountry.style = 'opacity: 0.5; font-weight: bold;';
 
-
-        // FORECAST DATA LOGIC
-        hourlyTitle.textContent = "Hourly Forecast";
+        const currentTempSup = document.createElement('sup');
+        const currentTempSpan = document.createElement('span');
+        currentTempSup.textContent = 'o';
+        currentTempSpan.textContent = 'C';
+        currentTemp.textContent = resolvedData.current.temp_c 
+        currentTemp.appendChild(currentTempSup)
+        currentTemp.appendChild(currentTempSpan)
+        const currentFeelingSup = document.createElement('sup');
+        const currentFeelingSpan = document.createElement('span');
+        currentFeelingSup.textContent = 'o';
+        currentFeelingSpan.textContent = 'C';
+        currentFeeling.textContent = 'Feels like ' + resolvedData.current.feelslike_c;
+        currentFeeling.style = 'opacity: 0.5; font-weight: bold;'
+        currentFeeling.appendChild(currentFeelingSup)
+        currentFeeling.appendChild(currentFeelingSpan)
+        currentDataIcon.src = 'https:'+resolvedData.current.condition.icon;
+        currentCondition.textContent = resolvedData.current.condition.text;
+        currentCondition.style = 'margin-left: 0';
         
-        hourlyHeader.appendChild(hourlyTitle);
-        hourlyHeader.appendChild(hourlyDate);
-        // const header = document.createElement('thead');
-        // const headerRow = document.createElement('tr');
+        speed.textContent = resolvedData.current.wind_kph + ' Km/h '+ resolvedData.current.wind_dir;
+        speedText.textContent = 'Wind';
+        speed.style = 'text-align: center';
+        speedText.style = 'opacity: 0.5; font-weight: bold; text-align: center;';
+        percent.textContent = resolvedData.current.humidity + '%';
+        percentText.textContent = 'Humidity';
+        percent.style = 'text-align: center';
+        percentText.style = 'opacity: 0.5; font-weight: bold; text-align: center;';
+        uv.textContent = resolvedData.current.uv;
+        uvText.textContent = 'UV Index';
+        uv.style = 'text-align: center';
+        uvText.style = 'opacity: 0.5; font-weight: bold; text-align: center;';
+        visibility.textContent = resolvedData.current.vis_km + ' Km';
+        visibilityText.textContent = 'Visibility';
+        visibilityText.style = 'opacity: 0.5; font-weight: bold; text-align: center;';
+        visibility.style = 'text-align: center';
+
+
+        // DAILY FORECAST DATA LOGIC
+        forecastDayAndDate.textContent = days[date.getDay() - 1] + ', '+ [dateArray[1], dateArray[2]].join(' ');
+        dailyIcon.src = 'https:' + day1.day.condition.icon;
+        dailyIcon.style = 'height: 100px; width: 100px; margin: 0 auto;';
+        const highTempSup = document.createElement('sup');
+        const highTempSpan = document.createElement('span');
+        highTempSup.textContent = 'o';
+        highTempSpan.textContent = 'C';
+        high.textContent = day1.day.maxtemp_c;
+        high.appendChild(highTempSup)
+        high.appendChild(highTempSpan);
+        const lowTempSup = document.createElement('sup');
+        const lowTempSpan = document.createElement('span');
+        lowTempSup.textContent = 'o';
+        lowTempSpan.textContent = 'C';
+        low.textContent = day1.day.mintemp_c;
+        low.style = 'opacity: 0.5; font-weight: bold;';
+        low.appendChild(lowTempSup)
+        low.appendChild(lowTempSpan)
+        
+
+        // HOURLY FORECAST DATA LOGIC
+        hourlyTitle.textContent = "Hourly Forecast";
+        hourlyForecast.style.display = "grid";
 
         const headerData = ['Time', 'Icon', 'Condition', 'Temp (Celcius)', 'Precipitation (mm)', 'Wind Speed (kph)', 'Humidity', 'Feels Like'];
         headerData.forEach(text => {
@@ -63,6 +146,8 @@ celcius.addEventListener('click', () => {
 
         header.appendChild(headerRow);
         hourlyData.appendChild(header);
+
+        
 
         day1.hour.forEach(hour => {
             const icon = document.createElement('img');
@@ -76,7 +161,6 @@ celcius.addEventListener('click', () => {
             const rowData6 = document.createElement('td');
             const rowData7 = document.createElement('td');
             const rowData8 = document.createElement('td');
-
             rowData1.textContent = hour.time.split(' ')[1];
             rowData2.appendChild(icon);
             rowData3.textContent = hour.condition.text;
@@ -101,7 +185,7 @@ celcius.addEventListener('click', () => {
         
         hourlyForecast.appendChild(hourlyHeader);
         hourlyForecast.appendChild(hourlyData);
-        hourlyForecast.style.display = "grid";
+        // hourlyForecast.style.display = "grid";
     })
     city = '';
 })
